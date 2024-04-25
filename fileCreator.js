dbb21b6b95514f837f7f660b48e20120a4a0ce2e
“Guada” <guadalupearroferia@gmail.com>
2024-04-25 20:50:31 +1000

    final upload

--- fileCreator.js
+++ fileCreator.js
@@ -1,41 +1,24 @@
 import fs, { constants } from "node:fs";
 import { Buffer } from "node:buffer";
 import prompt from "prompt-sync";
-
+import cls from "colors"
 
 const promptFunc = prompt()
 
 export const fileCreator= () => {
     let action;
     do {
-        action = promptFunc(cls.blue("Qué acción desea realizar? (Lectura: R, Edición: E, Creación: C, Eliminar: D, Salir: Q): "))
-    } while (!(action === "R" || action === "E" || action === "C" || action === "D" || action === "Q"));
-
+        action = promptFunc(cls.blue("Qué acción desea realizar? Escribir en mayusculas (Crear: C, Leer : R, Modificar: M, Eliminar: D, Salir: Q): "))
+    } while (!(action === "C" || action === "M" || action === "D" || action === "Q" || action === "R"));
     if (action === "Q") {
         return;
     }
 
-    const fileName = promptFunc(cls.blue("Por favor, ingrese el nombre del archivo: "))
-    const dirName = `./files/${fileName}`
-
     switch (action) {
-        case "R":
-            fs.access(
-                dirName,
-                constants.R_OK,
-                error => {
-                    if (error) {
-                        console.log(cls.red("Este archivo no se puede leer"))
-                    } else {
-                        const file = fs.readFileSync(dirName)
-                        const data = file.toString()
-                        console.log(cls.green(data))
-                    }
-                }
-            )
-            break;
         case "C":
-            const data = promptFunc(cls.blue("Por favor, ingrese el contenido del archivo: "))
+            const fileName = promptFunc(cls.rainbow("Por favor, ingrese el nombre del archivo: "))
+            const dirName = `./createdFiles/${fileName}`
+            const data = promptFunc(cls.yellow("Por favor, ingrese el contenido del archivo: "))
             const buffer = new Uint8Array(Buffer.from(data));
             fs.writeFileSync(dirName, buffer, error => {
                 if (error) {
@@ -45,17 +28,39 @@ export const fileCreator= () => {
             })
             
             break;
-        case "E":
+        case "R":
+            const fileToRead = promptFunc(cls.yellow("Por favor, ingrese el nombre del archivo que quiere leer: "));
+            const filePathToRead = `./createdFiles/${fileToRead}`;
             fs.access(
-                dirName,
-                constants.R_OK | constants.W_OK,
+                filePathToRead, fs.constants.F_OK, 
                 error => {
                     if (error) {
-                        console.log(cls.red("Este archivo no se puede leer y/o no se puede escribir."))
+                        console.log(cls.red("El archivo especificado no existe."));
                     } else {
-                        const finalData = promptFunc(cls.blue("Por favor, ingrese el contenido del archivo: "))
-                        const buffer = new Uint8Array(Buffer.from(finalData));
-                        fs.writeFileSync(dirName, buffer, error => {
+                        fs.readFile(filePathToRead, (err, data) => {
+                            if (err) {
+                                console.log(cls.red("Lo sentimos, no hemos podido leer el archivo."));
+                            } else {
+                                console.log(cls.green("Contenido del archivo:"));
+                                console.log(data.toString());
+                            }
+                        });
+                    }
+                })
+
+            break;
+        case "M":
+            const fileToEdit = promptFunc(cls.yellow("Por favor, ingrese el nombre del archivo a editar: "));
+            const filePathToEdit = `./createdFiles/${fileToEdit}`;
+            fs.access(
+                filePathToEdit, fs.constants.F_OK| constants.R_OK | constants.W_OK,
+                error => {
+                    if (error) {
+                        console.log(cls.red("Este archivo no existe"))
+                    } else {
+                        const fileData = promptFunc(cls.rainbow("Por favor, ingrese el nuevo contenido del archivo: "))
+                        const buffer = new Uint8Array(Buffer.from(fileData));
+                        fs.writeFileSync(filePathToEdit, buffer, error => {
                             if (error) {
                                 console.log(cls.red("Lo sentimos, no hemos podido modificar el archivo"))
                             }
@@ -66,11 +71,14 @@ export const fileCreator= () => {
             )
             break;
         case "D":
-            fs.rm(dirName, error => {
+            const fileToDelete = promptFunc(cls.yellow("Por favor, ingrese el nombre del archivo a eliminar: "));
+            const filePathToDelete= `./createdFiles/${fileToDelete}`;
+            fs.rm(filePathToDelete, error => {
                 if (error) {
                     console.log(cls.red("Lo sentimos, no hemos podido eliminar el archivo"))
+                }else{
+                    console.log(cls.green("El archivo se ha eliminado correctamente"))
                 }
-                console.log(cls.green("El archivo se ha eliminado correctamente"))
             })
             break;
         default:
