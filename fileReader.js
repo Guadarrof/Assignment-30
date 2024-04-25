13cd8d29550a141ffecd81591367101131bbc6b1
“Guada” <guadalupearroferia@gmail.com>
2024-04-25 19:48:30 +1000

    created file

---
+++ fileReader.js
@@ -0,0 +1,9 @@
+import fs from "node:fs"
+
+export const readFile = () => {
+    const file = fs.readFileSync("./ejemplo.txt");
+    const data = file.toString()
+    const rows = data.split("\n")
+    const matrix = rows.map( row => row.split(" "))
+    console.log(matrix)
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+import { fileCreator } from "./fileCreator.js"
+
+fileCreator()
